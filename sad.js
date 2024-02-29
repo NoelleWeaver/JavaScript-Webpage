e05ac5dcdee2c1f01ef98dc1fa5c79dcb4f21198
NoelleWeaver <141285591+NoelleWeaver@users.noreply.github.com>
2024-02-28 18:20:19 -0700

    a

---
+++ sad.js
@@ -0,0 +1,22 @@
+document.body.style.color = 'red'
+let nav = document.createElement('div')
+let home = document.createElement('a')
+let news = document.createElement('a')
+
+nav.style.backgroundcolor = '#333'
+nav.style.backgroundSize = 'cover'
+nav.style.overflow = 'hidden'
+
+home.style.float = 'left'
+home.style.color = '#en29uf'
+home.style.textAlign = 'center'
+home.style.padding = '14px 16px'
+home.style.textDecoration = 'none'
+home.style.fontsize = '17px'
+
+document.appendChild(nav)
+
+let gallery = document.createElement('div')
+let img1 = document.createElement('img')
+let img2 = document.createElement('img')
+let img3 = document.createElement('img')
