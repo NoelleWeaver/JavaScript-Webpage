8595f272904e3f1adaece2da6f38b66b63cef715
NoelleWeaver <141285591+NoelleWeaver@users.noreply.github.com>
2024-02-29 18:10:27 -0700

    a

--- sad.js
+++ sad.js
@@ -1,22 +1,58 @@
-document.body.style.color = 'red'
+// document.body.style.backgroundColor = 'red'
 let nav = document.createElement('div')
 let home = document.createElement('a')
 let news = document.createElement('a')
+let gallery = document.createElement('a')
 
-nav.style.backgroundcolor = '#333'
-nav.style.backgroundSize = 'cover'
+nav.style.backgroundColor = '#333'
+nav.style.padding = "20px"
 nav.style.overflow = 'hidden'
+nav.style.borderRadius = '5px'
+document.body.append(nav);
 
-home.style.float = 'left'
-home.style.color = '#en29uf'
+// home.style.float = 'left'
+home.style.position = 'relative'
+home.style.left = '10px'
+home.style.backgroundColor = '#fff'
+home.style.color = '#333'
+home.style.borderRadius = '10px'
 home.style.textAlign = 'center'
 home.style.padding = '14px 16px'
 home.style.textDecoration = 'none'
 home.style.fontsize = '17px'
+home.textContent = "Home"
+home.href = "google.com";
+nav.append(home);
 
-document.appendChild(nav)
+// gallery.style.float = 'left'
+gallery.style.position = 'relative'
+gallery.style.left = '20px'
+gallery.style.backgroundColor = '#fff'
+gallery.style.color = '#333'
+gallery.style.borderRadius = '10px'
+gallery.style.textAlign = 'center'
+gallery.style.padding = '14px 16px'
+gallery.style.textDecoration = 'none'
+gallery.style.fontsize = '17px'
+gallery.textContent = "Gallery"
+gallery.href = "google.com";
+nav.append(gallery);
 
-let gallery = document.createElement('div')
+// news.style.float = 'left'
+news.style.position = 'relative'
+news.style.left = '30px'
+news.style.backgroundColor = '#fff'
+news.style.color = '#333'
+news.style.textAlign = 'center'
+news.style.padding = '14px 16px'
+news.style.textDecoration = 'none'
+news.style.fontsize = '17px'
+news.style.borderRadius = '10px'
+news.textContent = "News"
+news.href = "google.com";
+nav.append(news);
+
+let gallery1 = document.createElement('div')
 let img1 = document.createElement('img')
 let img2 = document.createElement('img')
 let img3 = document.createElement('img')
